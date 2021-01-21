403e61940f5e053c3ab6994f810082fd75e54e48
VacuityBox <69965495+VacuityBox@users.noreply.github.com>
2021-01-21 01:36:39 -0800

    Add structures

---
+++ Src/Structures/Payload.cs
@@ -0,0 +1,42 @@
+﻿// ShowPlay - Show what song is playing
+//
+// Copyright (C) 2020-2021 VacuityBox
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// SPDX-License-Identifier: GPL-3.0-only 
+
+using System.Runtime.Serialization;
+
+namespace ShowPlay
+{
+    [DataContract]
+    public class Paylaod
+    {
+        [DataMember]
+        public uint Frame { get; set; }
+
+        [DataMember]
+        public PlayerInfo Player { get; set; } = null;
+
+        [DataMember]
+        public PlaybackInfo Playback { get; set; } = null;
+
+        [DataMember]
+        public SongInfo Song { get; set; } = null;
+
+        [DataMember]
+        public CoverInfo Cover { get; set; } = null;
+    }
+}
